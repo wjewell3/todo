48c00b58adf9a1c9f3f4ba2bd5c0c35712aeddf1
FYE7200 <FYE7200@CORPFYE7200MB.local>
2020-10-06 17:05:14 -0500

    droped db todo_dev, and then mix ecto.created it again

--- lib/todo_web/controllers/todo_controller.ex
+++ lib/todo_web/controllers/todo_controller.ex
@@ -4,23 +4,23 @@ defmodule TodoWeb.TodoController do
   alias Todo.Todo
 
   def index(conn, _params) do
-    todos = Repo.all(Todo)
-    render conn, "index.html", todos: todos
+    # todos = Repo.all(Todo)
+    render conn, "index.html" #, todos: todos
   end
 
-  def create(conn, params) do
-    %{"todo" => todo_param} = params
-    changeset = Todo.changeset(%Todo{}, todo_param)
+  # def create(conn, params) do
+  #   %{"todo" => todo_param} = params
+  #   changeset = Todo.changeset(%Todo{}, todo_param)
 
-    case Repo.insert(changeset) do
-      {:ok, _topic} ->
-        conn
-        |> put_flash(:info, "Task Created")
-        |> redirect(to: topic_path(conn, :index))
-      {:error, changeset} ->
-        render conn, "new.html", changeset: changeset
-    end
+  #   case Repo.insert(changeset) do
+  #     {:ok, _topic} ->
+  #       conn
+  #       |> put_flash(:info, "Task Created")
+  #       |> redirect(to: topic_path(conn, :index))
+  #     {:error, changeset} ->
+  #       render conn, "new.html", changeset: changeset
+  #   end
 
-    case
-  end
+  # end
+
 end
