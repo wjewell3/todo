064547a6de304400f68df82c581bd1b94f7532a8
FYE7200 <FYE7200@CORPFYE7200MB.local>
2020-10-07 12:42:04 -0500

    updated index page

--- lib/todo_web/views/todo_view.ex
+++ lib/todo_web/views/todo_view.ex
@@ -1,3 +1,10 @@
 defmodule TodoWeb.TodoView do
   use TodoWeb, :view
+  # add class "completed" to a todo if todo.status=1
+  def complete(todo) do
+    case todo.status do
+      1 -> "completed"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
 end
