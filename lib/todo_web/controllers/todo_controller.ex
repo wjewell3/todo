42ed1f928e548b935cd6dabf1827346564f4afee
FYE7200 <FYE7200@CORPFYE7200MB.local>
2020-10-06 22:19:09 -0500

    update

--- lib/todo_web/controllers/todo_controller.ex
+++ lib/todo_web/controllers/todo_controller.ex
@@ -4,23 +4,28 @@ defmodule TodoWeb.TodoController do
   alias Todo.Todo
 
   def index(conn, _params) do
-    # todos = Repo.all(Todo)
-    render conn, "index.html" #, todos: todos
+    todos = Repo.all(Todo)
+    render conn, "index.html", todos: todos
   end
 
-  # def create(conn, params) do
-  #   %{"todo" => todo_param} = params
-  #   changeset = Todo.changeset(%Todo{}, todo_param)
+  def new(conn, _params) do
+    changeset = Todo.changeset(%Todo{},%{})
+    render conn, "new.html", changeset: changeset
+  end
 
-  #   case Repo.insert(changeset) do
-  #     {:ok, _topic} ->
-  #       conn
-  #       |> put_flash(:info, "Task Created")
-  #       |> redirect(to: topic_path(conn, :index))
-  #     {:error, changeset} ->
-  #       render conn, "new.html", changeset: changeset
-  #   end
+  def create(conn, params) do
+    %{"todo" => todo_param} = params
+    changeset = Todo.changeset(%Todo{}, todo_param)
 
-  # end
+    case Repo.insert(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Task Created")
+        |> redirect(to: todo_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+
+  end
 
 end
