680b65605e72882e84dc55f8ead4da5a8fcd2bc5
FYE7200 <FYE7200@CORPFYE7200MB.local>
2020-10-09 09:48:41 -0500

    counter now working

--- lib/todo_web/live/counter_live.ex
+++ lib/todo_web/live/counter_live.ex
@@ -8,9 +8,21 @@ defmodule TodoWeb.CounterLive do
 
     def render(assigns) do
         ~L"""
-        <h1>Count: <% @count %></h1>
-        <button>+</button>
-        <button>-</button>
+        <h1>Count: <%= @count %></h1>
+        <button phx-click="increment">+</button>
+        <button phx-click="decrement">-</button>
         """
+    end
+
+    def handle_event("increment", _, socket) do
+        count = socket.assigns.count + 1
+        socket = assign(socket, :count, count)
+        {:noreply, socket}
+    end
+
+    def handle_event("decrement", _, socket) do
+        count = socket.assigns.count - 1
+        socket = assign(socket, :count, count)
+        {:noreply, socket}
     end
 end
